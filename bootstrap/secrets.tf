f6be303cc213e8290236f99e73e8304ea00b0ba4
Ihor Knysh <41473767+imknysh@users.noreply.github.com>
2026-03-25 04:37:12 +0000

    Add Inventory

--- bootstrap/secrets.tf
+++ bootstrap/secrets.tf
@@ -14,3 +14,22 @@ data:
   BEDROCK_API_KEY: "${base64encode(var.BEDROCK_API_KEY)}"
 YAML
 }
+
+resource "random_id" "inventory_key" {
+  byte_length = 16
+}
+
+resource "kubectl_manifest" "inventory" {
+    depends_on = [kind_cluster.this]
+
+    yaml_body = <<YAML
+apiVersion: v1
+kind: Secret
+metadata:
+  name: ai-inventory-secret
+  annotations:
+    replicator.v1.mittwald.de/replicate-to: "ai-inventory-system"
+data:
+  AGENT_REGISTRY_JWT_PRIVATE_KEY: "${base64encode(resource.random_id.inventory_key.hex)}"
+YAML
+}
